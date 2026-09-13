6eb7c28b656b4246a8f495ecc49194b41e855772
Alexander Minor <alexander@minor.pw>
2026-09-13 17:39:16 -0400

    created bringup package

--- robotiq_bringup/launch/bringup.launch.py
+++ robotiq_bringup/launch/bringup.launch.py
@@ -1,40 +1,12 @@
-# Copyright (c) 2022 PickNik, Inc.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#
-#    * Neither the name of the {copyright_holder} nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 import os
 import launch
-from launch.conditions import IfCondition
 from launch.substitutions import (
     Command,
     FindExecutable,
     LaunchConfiguration,
     PathJoinSubstitution,
 )
+from launch_ros.parameter_descriptions import ParameterFile
 import launch_ros
 
 
@@ -42,11 +14,11 @@ def generate_launch_description():
     description_pkg_share = launch_ros.substitutions.FindPackageShare(
         package="robotiq_description"
     ).find("robotiq_description")
+    bringup_pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="robotiq_bringup"
+    ).find("robotiq_description")
     default_model_path = os.path.join(
         description_pkg_share, "urdf", "robotiq_2f_85_gripper.urdf.xacro"
-    )
-    default_rviz_config_path = os.path.join(
-        description_pkg_share, "rviz", "view_urdf.rviz"
     )
 
     args = []
@@ -62,18 +34,6 @@ def generate_launch_description():
             name="model",
             default_value=default_model_path,
             description="Absolute path to gripper URDF file",
-        )
-    )
-    args.append(
-        launch.actions.DeclareLaunchArgument(
-            name="rvizconfig",
-            default_value=default_rviz_config_path,
-            description="Absolute path to rviz config file",
-        )
-    )
-    args.append(
-        launch.actions.DeclareLaunchArgument(
-            name="launch_rviz", default_value="false", description="Launch RViz?"
         )
     )
     args.append(
@@ -121,9 +81,8 @@ def generate_launch_description():
         ]
     )
 
-    controllers_file = "robotiq_controllers.yaml"
-    initial_joint_controllers = PathJoinSubstitution(
-        [description_pkg_share, "config", controllers_file]
+    ros2_controllers_file = PathJoinSubstitution(
+        [bringup_pkg_share, "config", "robotiq_controllers.yaml"]
     )
 
     control_node = launch_ros.actions.Node(
@@ -131,9 +90,9 @@ def generate_launch_description():
         executable="ros2_control_node",
         namespace=ns,
         parameters=[
+            ParameterFile(ros2_controllers_file, allow_substs=True),
             robot_description_param,
             update_rate_config_file,
-            initial_joint_controllers,
         ],
     )
 
@@ -142,16 +101,6 @@ def generate_launch_description():
         executable="robot_state_publisher",
         namespace=ns,
         parameters=[robot_description_param],
-    )
-
-    rviz_node = launch_ros.actions.Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        namespace=ns,
-        output="log",
-        arguments=["-d", LaunchConfiguration("rvizconfig")],
-        condition=IfCondition(LaunchConfiguration("launch_rviz")),
     )
 
     joint_state_broadcaster_spawner = launch_ros.actions.Node(
@@ -185,7 +134,6 @@ def generate_launch_description():
         joint_state_broadcaster_spawner,
         robotiq_gripper_controller_spawner,
         robotiq_activation_controller_spawner,
-        rviz_node,
     ]
 
     return launch.LaunchDescription(args + nodes)
